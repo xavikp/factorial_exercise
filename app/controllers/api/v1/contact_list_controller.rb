4f508d1fa3d938dd55199ea755da162c5622241d
xavier.val <1xaviervalparejo@gmail.com>
2022-03-18 17:53:04 +0100

    API methods
    Updated and completed API methods to complain with constraints

--- app/controllers/api/v1/contact_list_controller.rb
+++ app/controllers/api/v1/contact_list_controller.rb
@@ -2,40 +2,63 @@
 module Api
   module V1
     class ContactListController < ApplicationController
+      #base function to get all the records in the Contacts Table from the DB
       def index
         contacts = Contact.all
         render json: ContactSerializer.new(contacts).serialized_json
       end
 
+      #function to get the information of a contact based on the id that is passed
+      # returns a json with the information of the user or a 404 error if not found
       def show
-        contact = Contact.find_by(id: params[:id])
-        render json: ContactSerializer.new(contact).serialized_json
-      end
-
-      def create
-        contact = Contact.new(contact_params)
-        if contact.save
-          render json: ContactSerializer.new(contact).serialized_json
+        contact = Contact.where(id: params[:id])
+        if contact[0] == nil
+          render json: { error: "Not found" }, status: 404
         else
-          render json: { error: contact.errors.messages }, status: 500
+          render json: ContactSerializer.new(contact).serialized_json, status: 200
         end
       end
 
+      #function to create a new record in the DB, creates the new record with the parsed data
+      # If the email is already in the DB return a 403 error message
+      # If the contact can't be saved due to DB error returns a 500
+      def create
+        contacts = Contact.all
+        contact = Contact.new(contact_params)
+        new_email = contact_params[:email]
+        if contacts.find_by(email: new_email)
+          render json: { error: "Email already in DB" }, status: 403
+        else
+          if contact.save
+            render json: ContactSerializer.new(contact).serialized_json, status: 200
+          else
+            render json: { error: contact.errors.messages }, status: 500
+          end
+        end
+      end
+
+      #update a contact in the DB, first check if the record exists and if it does update the contact
       def update
         contact = Contact.find_by(email: params[:email])
-        if contact.update(contact_params)
-          render json: ContactSerializer.new(contact).serialized_json
+        if contact[0] == nil
+          render json: { error: "Not found" }, status: 404
         else
-          render json: { error: contact.errors.messages }, status: 405
+          if contact.update(contact_params)
+            render json: ContactSerializer.new(contact).serialized_json, status: 200
+          else
+            render json: { error: contact.errors.messages }, status: 405
+          end
         end
+
       end
 
+      #delete a contact in the DB
       def destroy
         contact = Contact.find_by(id: params[:id])
         if contact.destroy
           head :no_content
         else
-          render json: { error: contact.errors.messages }, status: 405
+          render json: { error: contact.errors.messages }, status: 404
         end
       end
 
--- app/controllers/api/v1/edits_controller.rb
+++ app/controllers/api/v1/edits_controller.rb
@@ -1,14 +1,47 @@
 module Api
   module V1
     class EditsController < ApplicationController
+      #base function to get all the records in the Edits Table from the DB
       def index
         edits = Edit.all
-        render json: EditSerializer.new(edits).serialized_json
+        render json: EditSerializer.new(edits).serialized_json, status: 200
       end
 
+      #function to get the information of an edit based on the contact_id that is passed
+      # returns a json with the information of the edits or a 404 error if not found
       def show
-        edit = Edit.first
-        render json: EditSerializer.new(edit).serialized_json
+        edit = Edit.where(contact_id: params[:id])
+        if edit[0] == nil
+          render json: { error: "Not found" }, status: 404
+        else
+          render json: EditSerializer.new(edit).serialized_json, status: 200
+        end
+      end
+
+      #function to create a new record in the DB, creates the new record with the parsed data
+      # If the contact can't be saved due to DB error returns a 500
+      def create
+        edit = Edit.new(edit_params)
+        if edit.save
+          render json: EditSerializer.new(edit).serialized_json, status: 200
+        else
+          render json: { error: edit.errors.messages }, status: 500
+        end
+      end
+
+      #destroys a record in the DB
+      def destroy
+        edit = Edit.where(contact_id: params[:id])
+        if edit.destroy_all
+          head :no_content
+        else
+          render json: {error: edit.errors.messages }, status: 404
+        end
+      end
+
+      def edit_params
+        params.permit(:date, :previous_first_name, :previous_last_name, :previous_phone_number,
+                      :previous_email, :history, :contact_id)
       end
     end
   end
